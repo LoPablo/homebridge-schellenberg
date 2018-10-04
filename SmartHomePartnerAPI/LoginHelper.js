39d33cbd181fdb5a9e97bc0739092f03253cb68c
Pascal Hartmann <pascal-hartmann@live.de>
2018-10-04 23:24:54 +0200

    compacted LoginHelper

--- SmartHomePartnerAPI/LoginHelper.js
+++ SmartHomePartnerAPI/LoginHelper.js
@@ -1,15 +1,13 @@
-const crypto = require('crypto');
+const crypt = require('crypto');
 const base64 = require('base-64');
 
 class LoginHelper {
     static calculateDigest(password, salt, sessionSalt) {
-        var hashedPassword = this.getPasswordHash(password, salt);
-        var finishedDigest = this.getDigest(hashedPassword, sessionSalt);
-        return finishedDigest;
-
+        const hashedPassword = this.getHash('sha256', password, salt);
+        return this.getHash('sha1', hashedPassword, sessionSalt);
     }
 
-    // https://stackoverflow.com/questions/3195865      modified
+    // https://stackoverflow.com/questions/3195865      modified because of javascript handling chars differently
     static string2Bin(str) {
         let result = [];
         for (let i = 0; i < str.length; i++) {
@@ -23,56 +21,13 @@ class LoginHelper {
         return result;
     }
 
-    // https://stackoverflow.com/questions/3195865     turned around
-    static bin2String(array) {
-        let result = '';
-        for (let i = 0; i < array.length; i++) {
-            let p = String.fromCharCode(array[i]);
-            result += p;
-            //if (p>128){
-            //   result.push(p-256);
-            //}else {
-            //    result.push(p);
-            //}
-        }
-        return result;
-
-        return String.fromCharCode.apply(String, array);
-    }
-
-    static getDigest(password, sessionSalt) {
-        let digest = this.getHashSHA1(password, sessionSalt);
-        return digest;
-    }
-
-    static getPasswordHash(password, salt) {
-        let digest = this.getHashSHA256(password, salt);
-        console.log('getPasswordHash: ' + digest);
-        return digest;
-    }
-
-    static getHashSHA1(password, salt) {
+    static getHash(method, password, salt) {
         let decode = base64.decode(salt);
         let saltArray = this.string2Bin(decode);
         let passwordArray = this.string2Bin(password);
         let pasConSalt = passwordArray.concat(saltArray);
-        let shacrypt = crypto.createHash('sha1').update(new Uint8Array(pasConSalt));
-        let fin = shacrypt.digest('base64');
-
-        console.log('getHashSHA1:' + fin);
-        return fin;
-    }
-
-    static getHashSHA256(password, salt) {
-        let decode = base64.decode(salt);
-        let saltArray = this.string2Bin(decode);
-        let passwordArray = this.string2Bin(password);
-        let pasConSalt = passwordArray.concat(saltArray);
-        let shacrypt = crypto.createHash('sha256').update(new Uint8Array(pasConSalt));
-        let fin = shacrypt.digest('base64');
-
-        console.log('getHashSHA256:' + fin);
-        return fin;
+        let cryptHash = crypt.createHash(method).update(new Uint8Array(pasConSalt));
+        return cryptHash.digest('base64');
     }
 }
 
